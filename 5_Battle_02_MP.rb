7fa7def595bb45e80109aa6175459007bdb0ef62
Quentin DECHAMBRE <quentin.dechambre@sigma-clermont.fr>
2025-07-30 02:43:48 +0200

    Ajout des commentaires des fonctions et variables

--- 5_Battle_02_MP.rb
+++ 5_Battle_02_MP.rb
@@ -1,6 +1,8 @@
 ###############TEST###############
 class Battle::Visual
   # Associe les backgrounds à leurs grounds correspondants
+  # Si le background n'est pas trouvé, on utilise 'ground_grass' par défaut
+  # Ces noms doivent correspondre aux noms des fichiers dans le dossier "battleback"  
   GROUND_BY_BATTLEBACK = {
     'back_grass' => 'ground_grass',
     'back_tall_grass' => 'ground_grass',
@@ -16,6 +18,10 @@ class Battle::Visual
     'back_ice' => 'ground_ice',
     'back_snow' => 'ground_snow'
   }
+  # Associe les backgrounds à leurs foregrounds correspondants
+  # Si le background n'est pas trouvé, on utilise 'fg_default' par défaut
+  # Ces noms doivent correspondre aux noms des fichiers dans le dossier "battleback"
+  # Note: Les foregrounds sont des éléments qui apparaissent au-dessus du background
 FOREGROUND_BY_BATTLEBACK = {
     'back_grass' => 'fg_grass',
     'back_tall_grass' => 'fg_tall_grass',
@@ -35,11 +41,13 @@ FOREGROUND_BY_BATTLEBACK = {
     @ground_sprites = {}
 
     # Récupère le nom de base du background actuel
+    # Utilise la fonction définie dans 05_Battle_02_MP.rb pour obtenir le nom du battleback, tout se joue à partir de lui !
     current_back = current_battleback_name
 
     # Cherche le ground associé dans le dictionnaire
+    # Si le background n'est pas trouvé, on utilise 'ground_grass' par défaut
     ground_name = GROUND_BY_BATTLEBACK[current_back] || 'ground_grass' # fallback par défaut
-
+    
     @battlers.each do |bank, sprites|
       sprites.each do |position, sprite|
         # On ne garde que les sprites de Pokémon
@@ -75,6 +83,7 @@ FOREGROUND_BY_BATTLEBACK = {
   end
   # Renvoie le nom exact du battleback actuellement utilisé (sans extension)
   # @return [String]
+
   def current_battleback_name
     @scene.battle_info.find_background_name_to_display do |filename|
       RPG::Cache.battleback_exist?(filename) || Yuki::GifReader.exist?("#{filename}.gif", :battleback)
@@ -83,9 +92,22 @@ FOREGROUND_BY_BATTLEBACK = {
 end
 
 class Battle::Scene
+  # Renvoie la position de base du sprite allié ou ennemi à partir des positions issues de Figma
+  # Changer seulement les valeurs de pos_x_ally et pos_x_enemy et pos_y pour modifier la position de base (prendre valeur de position de Figma)
+  # @return [Integer, Integer] Position X et Y du sprite
+  def base_position_v1
+    pos_x_ally = 16  # Position X par défaut de l'allié
+    pos_x_enemy = 208 # Position X par défaut de l'ennemi
+    pos_y = 100 # Position Y par défaut des deux sprites
 
+    return pos_x_enemy, pos_y if enemy?
+    return pos_x_ally, pos_y
+  end
 
+  # Renvoie la position de base du sprite allié ou ennemi pour la version custom
+  # @return [Integer, Integer] Position X et Y du sprite
   def base_position_v1_custom
+
     pos_x_ally= 16  # Position X par défaut de l'allié
     pos_x_enemy = 208 # Position X par défaut de l'ennemi
     pos_y = 100 # Position Y par défaut des deux sprites
