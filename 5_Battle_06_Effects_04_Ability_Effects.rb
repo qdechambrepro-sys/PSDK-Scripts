c6c3177b57d7d98b64d4d870d8eb564eba3c21e3
Quentin DECHAMBRE <quentin.dechambre@sigma-clermont.fr>
2025-08-02 01:12:53 +0200

    Ajout du talents Chaos Entropique

--- 5_Battle_06_Effects_04_Ability_Effects.rb
+++ 5_Battle_06_Effects_04_Ability_Effects.rb
@@ -337,6 +337,20 @@ module Battle
       register(:solid_rock, SuperEffectivePowerReduction)
       register(:filter, SuperEffectivePowerReduction)
       register(:prism_armor, SuperEffectivePowerReduction)
+
+      class SupersuperEffectivePowerReduction < Ability
+        # Give the move mod3 mutiplier (after everything)
+        # @param user [PFM::PokemonBattler] user of the move
+        # @param target [PFM::PokemonBattler] target of the move
+        # @param move [Battle::Move] move
+        # @return [Float, Integer] multiplier
+        def mod3_multiplier(user, target, move)
+          return 1 if target != @target
+          return 1 unless user.can_be_lowered_or_canceled? && move.super_effective?
+          return 0.5
+        end
+      end
+      register(:chaos_entropique, SupersuperEffectivePowerReduction)
       class Drizzle < Ability
         # Function called when a Pokemon has actually switched with another one
         # @param handler [Battle::Logic::SwitchHandler]
